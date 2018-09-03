c30318713a9a1d52e8e2374ded9e11e22eb5ae48
박상윤 <mim3431@naver.com>
2018-09-03 10:53:44 +0900

    잠시 기록용
    monotone chain(기하) 코드 잠시 기록

--- main.c
+++ main.c
@@ -1,4 +1,45 @@
-#include <stdio.h>
+#include<stdio.h>
+#include<algorithm>
+#include<vector>
+
+using namespace std;
+
+struct Point {
+	int x, y;
+	Point operator -(const Point A)const {
+		return{ x - A.x,y - A.y };
+	}
+	bool operator <(const Point A)const {
+		return x == A.x ? y<A.y : x<A.x;
+	}
+};
+
+int ccw(Point A, Point B) {
+	return A.x*B.y - B.x*A.y;
+}
+int ccw(Point A, Point B, Point C) {
+	int t = ccw(B - A, C - A);
+	return t<0 ? -1 : t>0;
+}
+
+int N;
+Point d[10001];
+vector<Point> uc, dc;
+int us;
+
+void monotoneChain() {
+	uc.push_back(d[1]);
+	uc.push_back(d[2]);
+	us = 2;
+	for (int i = 3; i <= N; i++) {
+		while (us >= 2 && ccw(uc[us - 2], uc[us - 1], d[i]) >= 0) {
+			uc.pop_back();
+			us--;
+		}
+		uc.push_back(d[i]);
+		us++;
+	}
+}
 
 int main()
 {
